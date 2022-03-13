84b96a9010464eafd04182b1074dc8c3e139be90
Jean-Frédéric Faust <jean.frederic.faust@gmail.com>
2022-03-13 12:49:21 -0400

    First draft (#1)

--- main.ts
+++ main.ts
@@ -1,81 +1,44 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, debounce, FileSystemAdapter, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface VarConfig {
+	vaultPath: string;
+	name: string;
+	value: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface VariablesPluginSettings {
+	filter: string;
+	variables: VarConfig[];
+	showApplicableVars: boolean;
+	applicableVarIndexes: number[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: VariablesPluginSettings = {
+	filter: "",
+	variables: [{
+		vaultPath: "*",
+		name: "demo",
+		value: "swapped"
+	}],
+	showApplicableVars: false,
+	applicableVarIndexes: [0]
+}
+
+export default class VariablesPlugin extends Plugin {
+	settings: VariablesPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerMarkdownPostProcessor((element, context) => {
+			for (let i = 0; i < this.settings.applicableVarIndexes.length; i++) {
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				const variable = this.settings.variables[this.settings.applicableVarIndexes[i]];
+				element.innerHTML = element.innerHTML.replaceAll(variable.name, variable.value);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new VariablesSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,47 +54,149 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+function getVaultAbsolutePath(app: App) {
+	let adapter = app.vault.adapter;
+	if (adapter instanceof FileSystemAdapter) {
+		return adapter.getBasePath();
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	return null;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class VariablesSettingTab extends PluginSettingTab {
+	plugin: VariablesPlugin;
+	debouncedRefresh = debounce(() => { this.display(); document.getElementById("plugin-vars-filter-input").focus(); }, 700, true);
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: VariablesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		let pinIcon = this.plugin.settings.showApplicableVars ? "check-in-circle" : "check-small";
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setClass("plugin-vars-header")
+			.setName("Variables")
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Filter by variable name')
+				.setValue(this.plugin.settings.filter)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.filter = value;
 					await this.plugin.saveSettings();
-				}));
+					this.debouncedRefresh();
+				}).inputEl.id = "plugin-vars-filter-input")
+			.addButton(btn => btn
+				.setTooltip("Open documentation on GitHub")
+				.setIcon("help")
+				.onClick(() => {
+					window.open("https://github.com/jffaust/obsidian-variables", '_blank');
+				})
+			)
+			.addButton(btn => btn
+				.setTooltip("Copy current vault path")
+				.setIcon("vault")
+				.onClick(() => {
+					navigator.clipboard.writeText(getVaultAbsolutePath(this.app));
+				})
+			)
+			.addButton(btn => btn
+				.setTooltip("Toggle between showing all variables or only the ones that apply to the current vault")
+				.setIcon(pinIcon)
+				.onClick(() => {
+					this.plugin.settings.showApplicableVars = !this.plugin.settings.showApplicableVars;
+					this.display();
+				})
+			)
+			.addButton(btn => btn
+				.setTooltip("Add a new variable")
+				.setIcon("import-glyph")
+				.onClick(() => {
+					this.plugin.settings.variables.push({
+						vaultPath: "",
+						name: "",
+						value: ""
+					});
+					this.display();
+				})
+			);
+
+		for (let i = 0; i < this.plugin.settings.variables.length; i++) {
+			const variable = this.plugin.settings.variables[i];
+
+			if (this.plugin.settings.filter && !variable.name.includes(this.plugin.settings.filter)) {
+				continue;
+			}
+
+			if (this.plugin.settings.showApplicableVars && !this.plugin.settings.applicableVarIndexes.contains(i)) {
+				continue;
+			}
+
+			new Setting(containerEl)
+				.setClass("plugin-vars-list")
+				.setName(`Variable`)
+				.addText(text => text
+					.setPlaceholder('vault path')
+					.setValue(this.plugin.settings.variables[i].vaultPath)
+					.onChange(async (value) => {
+						console.log('Updated var vault path: ' + value);
+						this.plugin.settings.variables[i].vaultPath = value;
+						this.updateApplicableVars();
+						await this.plugin.saveSettings();
+					}))
+				.addText(text => text
+					.setPlaceholder('name')
+					.setValue(this.plugin.settings.variables[i].name)
+					.onChange(async (value) => {
+						console.log('Updated var name: ' + value);
+						this.plugin.settings.variables[i].name = value;
+						this.updateApplicableVars();
+						await this.plugin.saveSettings();
+					}))
+				.addText(text => text
+					.setPlaceholder('value')
+					.setValue(this.plugin.settings.variables[i].value)
+					.onChange(async (value) => {
+						console.log('Updated var value: ' + value);
+						this.plugin.settings.variables[i].value = value;
+						await this.plugin.saveSettings();
+					}))
+				.addExtraButton(btn => btn
+					.setIcon("cross-in-box")
+					.setTooltip("Delete this variable")
+					.onClick(() => {
+						this.plugin.settings.variables.splice(i, 1);
+						this.display();
+					})
+				);
+
+		}
+
+	}
+
+	updateApplicableVars(): void {
+		let newIndexesMap: { [key: string]: number } = {};
+		for (let i = 0; i < this.plugin.settings.variables.length; i++) {
+			let v = this.plugin.settings.variables[i];
+
+			if (v.name && v.vaultPath) {
+				if (newIndexesMap[v.name] == undefined) {
+					if (v.vaultPath == "*" || v.vaultPath == getVaultAbsolutePath(this.app)) {
+						newIndexesMap[v.name] = i;
+					}
+				} else {
+
+					let currentVar = this.plugin.settings.variables[newIndexesMap[v.name]];
+					if (currentVar.vaultPath == "*" && v.vaultPath == getVaultAbsolutePath(this.app)) {
+						newIndexesMap[v.name] = i;
+					}
+				}
+			}
+		}
+
+		this.plugin.settings.applicableVarIndexes = Object.values(newIndexesMap);
 	}
 }
